3e744aba8fb82407286e0bf9f51efc3669d5fae2
luispablom <165343569+luispablom@users.noreply.github.com>
2024-04-05 22:26:49 -0300

    Update scripts.js
    smooth

--- scripts.js
+++ scripts.js
@@ -20,3 +20,12 @@ jQuery(document).ready(function ($) {
     }
   });
 });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
